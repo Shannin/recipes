f98636f65195c93ce24562e85d2587ebb6bce8ac
Shannin Ciprich <shanninc@gmail.com>
2016-08-21 20:26:14 -0700

    add basic form contents

--- app/routes/recipe/new.js
+++ app/routes/recipe/new.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model(params) {
+        return {
+            title: "some title",
+            description: "",
+            create_date: "",
+            ingredients: [
+                {id: 1, name: 'Whole Wheat Flour', category: 'flours', unit: 'g'},
+                {id: 2, name: 'Water', category: 'liquid', unit: 'g'},
+                {id: 3, name: 'Salt', category: 'additive', unit: 'g'},
+                {id: 4, name: 'Yeast', category: 'additive', unit: 'g'}
+            ],
+            photos: {},
+            steps: {}
+        };
+    }
 });
