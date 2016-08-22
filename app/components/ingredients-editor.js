22933982abcea6618f98845adda79f700b6ced09
Shannin Ciprich <shanninc@gmail.com>
2016-08-21 20:54:07 -0700

    ingredients-editor basic structure

--- app/components/ingredients-editor.js
+++ app/components/ingredients-editor.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+    actions: {
+        add() {
+            console.log("can I log??");
+        },
+        
+        remove(ingredient) {
+            console.log("remove this ingredient: " + ingredient);
+        }
+    }
+    
 });
--- app/routes/recipe/new.js
+++ app/routes/recipe/new.js
@@ -4,13 +4,15 @@ export default Ember.Route.extend({
     model(params) {
         return {
             title: "some title",
-            description: "",
-            create_date: "",
+            description: '',
+            yield: 2,
+            yield_label: 'loaves',
+            create_date: '',
             ingredients: [
-                {id: 1, name: 'Whole Wheat Flour', category: 'flours', unit: 'g'},
-                {id: 2, name: 'Water', category: 'liquid', unit: 'g'},
-                {id: 3, name: 'Salt', category: 'additive', unit: 'g'},
-                {id: 4, name: 'Yeast', category: 'additive', unit: 'g'}
+                {id: 1, qty: '1000', name: 'Whole Wheat Flour', category: 'flours', unit: 'g'},
+                {id: 2, qty: '880',  name: 'Water', category: 'liquid', unit: 'g'},
+                {id: 3, qty: '12',   name: 'Salt', category: 'additive', unit: 'g'},
+                {id: 4, qty: '5',    name: 'Yeast', category: 'additive', unit: 'g'}
             ],
             photos: {},
             steps: {}
