c3b3269b9049b0f8cad56ef4f5fe3721209212ed
Shannin Ciprich <shanninc@gmail.com>
2016-08-21 18:02:01 -0700

    add recipe/new route

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('recipe');
+  this.route('recipe', function() {
+    this.route('new');
+  });
   this.route('ingredients');
 });
 
